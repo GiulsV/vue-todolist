f5b92b917140fac9a34d47857623acf0b0d4c3a8
GiuliaV <valente.giulia93@gmail.com>
2022-05-26 15:53:35 +0200

    Stampa lista a video e x rimuove elemento

--- script/script.js
+++ script/script.js
@@ -3,12 +3,64 @@
 // - text, una stringa che indica il testo del todo
 // - done, un booleano (true/false) che indica se il todo è stato fatto oppure no
 
-// MILESTONE 1
-// Stampare all’interno di una lista, un item per ogni todo.
-// Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
 
-// MILESTONE 2
-// Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il todo viene rimosso dalla lista.
+const todos = [
+    {
+        text: 'Fare la spesa',
+        done: true
+    },
+    {
+        text: 'Studiare',
+        done: false
+    },
+    {
+        text: 'Prenotare Biglietti',
+        done: true
+    },
+    {
+        text: 'Cucinare',
+        done: false
+    }
+]
+
+const app = new Vue({
+
+    el: '#todo',
+    data: {
+        todos,
+        doneToDos: [],
+        newToDo: {
+            text: '',
+            done: false
+        }
+    },
+
+
+    // MILESTONE 1
+    // Stampare all’interno di una lista, un item per ogni todo.
+    // Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+    
+    methods: {
+
+        addItem(){
+            if(this.newToDo !== ""){
+                this.todos.push(this.newToDo);
+                this.newToDo = "";
+
+            }
+        },
+
+
+        // MILESTONE 2
+        // Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il todo viene rimosso dalla lista.
+
+        deleteItem(index){
+            this.todos.splice(index,1);
+        }
+    }
+});
+    
+
 
 // MILESTONE 3
 // Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
